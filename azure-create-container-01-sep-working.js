f2eca76e20dec0f236585df2563ab27d5dce5293
AshishTechRBM <ashish.saini@techrbm.com>
2025-05-14 17:32:57 +0530

    Update azure-create-container-01-sep-working.js

--- azure-create-container-01-sep-working.js
+++ azure-create-container-01-sep-working.js
@@ -1,7 +1,7 @@
 const { DefaultAzureCredential } = require("@azure/identity");
 const { ContainerInstanceManagementClient } = require("@azure/arm-containerinstance");
 
-const subscriptionId = 'da1fcdce-4d95-44a0-a4e7-0f0b53c6bc26'; // Replace with your Azure subscription ID
+const subscriptionId = '00000000-0000-0000-0000-000000000000'; // Replace with your Azure subscription ID
 const resourceGroupName = 'Fanmire_Platform'; // Replace with your resource group name
 const containerGroupName = 'ashishonlive'; // Replace with your container group name
 
@@ -9,7 +9,7 @@ const containerGroupName = 'ashishonlive'; // Replace with your container group 
 const acrServer = 'fanmireregistry.azurecr.io'; // e.g., 'myregistry.azurecr.io'
 const imageName = 'fanmire-streaming:v1'; // e.g., 'myimage:latest'
 const acrUsername = 'FanmireRegistry'; // ACR username or '00000000-0000-0000-0000-000000000000' for managed identity
-const acrPassword = '0auphf2xNLEiHPcBW5GMUk18XFnlh/ZdhDb37bOErU+ACRBPT1Zw'; // ACR password or access key
+const acrPassword =  // ACR password or access key
 
 async function main() {
   // Create a credential object using DefaultAzureCredential (which supports multiple authentication methods)
